da0ac49c9a9fc4dceb85eb508d579fb276fb8006
Mario Preishuber <mario.preishuber@stud.sbg.ac.at>
2014-10-10 17:51:14 +0200

    add ausschreibung

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
  	 * @author	Mario Preishuber <preishuber.mario@gmail.com>
  	 */
 	namespace IMSC;
-	
+
 	use \IMSC\php\HtmlTags;
 	use \IMSC\php\MobileDetect;
 	use \IMSC\php\Facebook\Informations;
-	
+
 	require_once 'php/HtmlTags.php';
 	require_once 'php/MobileDetect.php';
 	require_once 'php/Facebook/Informations.php';
-	
+
 	$htmltags = new HtmlTags();
 	$fb_infos = new Informations();
 	$detect = new MobileDetect();
@@ -26,20 +26,20 @@
 		<meta name="apple-mobile-web-app-capable" content="yes"/>
 		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
 		<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-		
+
 		<link rel="shortcut icon" href="../imgs/favicon.png">
 		<link rel="stylesheet" href="../css/bootstrap.min.css">
 		<!-- <link rel="stylesheet" href="../css/bootstrap-theme.min.css"> -->
-		
+
 		<link href="../css/lightbox_dark.css" rel="stylesheet">
-		
+
 		<script type="text/javascript" src="../js/jquery.min.js"></script>
 		<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="../js/lightbox.js"></script>
-	
+
 		<link rel="stylesheet" href="../css/style.css">
 	</head>
-	
+
 	<body onresize="resizeSections()">
 		<!-- ************************************************************************************ -->
 		<!-- * NAVIGATION BAR -->
@@ -91,16 +91,16 @@
 						<a href="#contact" data-toggle="collapse" data-target=".nav-collapse">
 							KONTAKT
 						</a>
-					</li>					
+					</li>
 				</ul>
 			</div>
 		</nav> <!-- end navigation -->
-		                             		
+
 		<!-- ************************************************************************************ -->
 		<!-- * CONTENT -->
 		<!-- ************************************************************************************ -->
 		<div class="page-wrapper">
-			
+
 		<!-- ************************************************************************************ -->
 		<!-- * HOME -->
 		<!-- ************************************************************************************ -->
@@ -117,7 +117,7 @@
 						</div>
 					</div>
 					<!-- important informations -->
-					
+
 					<div class="col-sm-6 col-sm-offset-1">
 						<div class="fb-feed-background">&nbsp;</div>
 						<div class="my_section fb-feed">
@@ -128,29 +128,55 @@
 								<h4>Badesee Parkplatz</h4>
 								<h3 class="btn btn-info">12. Oktober 2014</h3>
 								<h4>Start 09:00 Uhr</h4>
+
+								<a href="./aus_08_imsc_ried.pdf" target="_blank">Ausschreibung</a><br />
+								<br />
+								Vorgezogener Start:<br />
+								<table>
+									<tr>
+										<td>Startzeit / Div / Klasse</td>
+										<td>Startnr.</td>
+										<td>Name</td>
+										<td>Verein</td>
+										<td>Auto</td>
+									</tr>
+									<tr>
+										<td>09:00 Uhr / 1 / 1+2</td>
+										<td>815</td>
+										<td>Roland T&uuml;rkis</td>
+										<td>SK V&ouml;st-Motorsport</td>
+										<td>Subaru Impreza WRX STI</td>
+									</tr>
+									<tr>
+										<td>10:00 Uhr / 1 / 3+4</td>
+										<td>911</td>
+										<td>Roland T&uuml;rkis</td>
+										<td>SK V&ouml;st-Motorsport</td>
+										<td>Subaru Impreza WRX STI</td>
+									</tr>
 							</div>
 						</div>
 					</div>
-				
+
 					<!-- event calendar -->
 					<!--
 					<div class="col-sm-6 col-sm-offset-1 player-wrapper">
 						<div class="player">
-							<iframe 								src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=imsc.ried%40gmail.com&amp;color=%23182C57&amp;src=4lbippnpc3kecmdmlcfmu7c0b4%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=1q0fnqg9mnib8nebph0d6aaop4%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FVienna" 
+							<iframe 								src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=imsc.ried%40gmail.com&amp;color=%23182C57&amp;src=4lbippnpc3kecmdmlcfmu7c0b4%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=1q0fnqg9mnib8nebph0d6aaop4%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FVienna"
 								style=" border-width:0;
-								opacity: 60; 
+								opacity: 60;
 								filter: alpha(opacity=6000);
     							opacity: 0.6;
-    							filter: alpha(opacity=60);" 
-								width="400" 
-								height="600" 
-								frameborder="0" 
+    							filter: alpha(opacity=60);"
+								width="400"
+								height="600"
+								frameborder="0"
 								scrolling="no" class="calendar">
 							</iframe>
 						</div>
 					</div>
 					-->
-					
+
 					<!-- player / youtube video -->
 					<!--
 					<div class="col-sm-6 col-sm-offset-1 player-wrapper">
@@ -160,15 +186,15 @@
 						</div>
 					</div>
 					-->
-						
+
 					<!-- empty box -->
 					<div class="col-sm-6 col-sm-offset-1 player-wrappe">&nbsp;</div>
 				</div>
 				<div id="fb-feeds" class="row" style="margin-top:10px">
 					<div class="col-sm-5 btn btn-default">
 						<div class="my_section fb-feed">
-							<div class="fb-like" data-href="https://www.facebook.com/imsc.ried" 
-								data-layout="standard" data-action="like" data-show-faces="true" 
+							<div class="fb-like" data-href="https://www.facebook.com/imsc.ried"
+								data-layout="standard" data-action="like" data-show-faces="true"
 								data-share="true" data-width="300">
 							</div>
 						</div>
@@ -176,7 +202,7 @@
 				</div>
 			</div>
 		</section><!-- end section home -->
-		
+
 		<!-- ************************************************************************************ -->
 		<!-- * NEWS -->
 		<!-- ************************************************************************************ -->
@@ -187,9 +213,9 @@
 					<h3 class="btn btn-danger">News</h3>
 				</div>
 			</div>
-			<?php print( $htmltags->getFacebookFeed() );  ?> 
+			<?php print( $htmltags->getFacebookFeed() );  ?>
 		</section><!-- end section news -->
-		
+
 		<!-- ************************************************************************************ -->
 		<!-- * CALENDAR -->
 		<!-- ************************************************************************************ -->
@@ -201,24 +227,24 @@
 				</div>
 				<br /><br />
 				<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23e3e9ff&amp;src=imsc.ried%40gmail.com&amp;color=%23182C57&amp;src=4lbippnpc3kecmdmlcfmu7c0b4%40group.calendar.google.com&amp;color=%23A32929&amp;src=1q0fnqg9mnib8nebph0d6aaop4%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" class="cal_div cal_div_month cal" frameborder="0" scrolling="no"></iframe>
-			 	
+
 				<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=imsc.ried%40gmail.com&amp;color=%23182C57&amp;src=4lbippnpc3kecmdmlcfmu7c0b4%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=1q0fnqg9mnib8nebph0d6aaop4%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=Europe%2FVienna" class="cal_div cal_div_events cal_events" frameborder="0" scrolling="no"></iframe>
-			
-			
+
+
 			</div> <!-- end container -->
 		</section> <!-- end section calendar -->
-		
+
 		<!-- ************************************************************************************ -->
 		<!-- * MEMBERS -->
 		<!-- ************************************************************************************ -->
 		<section class="my_section" id="members">
 			<div class="background">&nbsp;</div>
 			<div class="container text-center" id="fb-feeds">
-			
+
 				<div class="col-sm-12 text-center">
 					<h3 class="btn btn-danger">Vorstand</h3>
 				</div>
-			
+
 				<!-- praesisent / obmann -->
 				<div class="row">
 					<div class="col-sm-6 ">
@@ -227,14 +253,14 @@
 							<br />&nbsp;
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/kraxenberger_franz.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/kraxenberger_franz.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Pr&auml;sident: Franz Kraxenberger">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/kraxenberger_franz.jpg"/>
 							</a>
 							<h6>Franz Kraxenberger</h6>
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/anonym.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/anonym.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Pr&auml;sident: Franz Kreuzhuber">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/anonym.jpg"/>
 							</a>
@@ -247,14 +273,14 @@
 							<br />&nbsp;
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/weilbold_guenter.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/weilbold_guenter.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Obmann: G&uuml;nter Weilbold">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/weilbold_guenter.jpg"/>
 							</a>
 							<h6>G&uuml;nter Weilbold</h6>
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/preishuber_guenther.jpg" data-toggle="lightbox" 
+							<a href="./imgs/persons/preishuber_guenther.jpg" data-toggle="lightbox"
 								data-gallery="multiimages" data-title="Obmann-Stellvertreter: G&uuml;nter Preishuber">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/preishuber_guenther.jpg"/>
 							</a>
@@ -262,7 +288,7 @@
 						</div>
 					</div>
 				</div>
-				
+
 				<!-- amateursportleiter / kassier -->
 				<div class="row">
 					<div class="col-sm-6">
@@ -291,7 +317,7 @@
 							<br />&nbsp;
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/preishuber_guenther.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/preishuber_guenther.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Kassier: G&uuml;nther Preishuber">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/preishuber_guenther.jpg"/>
 							</a>
@@ -306,7 +332,7 @@
 						</div>
 					</div>
 				</div>
-				
+
 				<!-- schriftfuehrer / zeugwert -->
 				<div class="row">
 					<div class="col-sm-6">
@@ -314,16 +340,16 @@
 							<h4 class="btn btn-default">Schriftf&uuml;hrer | Stellvertreter</h4>
 							<br />&nbsp;
 						</div>
-						
+
 						<div class="col-sm-6">
-							<a href="./imgs/persons/salomon_erwin.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/salomon_erwin.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Schriftf&uuml;hrer: Erwin Salomon">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/salomon_erwin.jpg"/>
 							</a>
 							<h6>Erwin Salomon</h6>
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/wagner_roland.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/wagner_roland.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Schriftf&uuml;hrer-Stellvertreter: Roland Wagner">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/wagner_roland.jpg"/>
 							</a>
@@ -336,14 +362,14 @@
 							<br />&nbsp;
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/wagner_anton.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/wagner_anton.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Zeugwart: Anton Wagner">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/wagner_anton.jpg"/>
 							</a>
 							<h6>Anton Wagner</h6>
 						</div>
 						<div class="col-sm-6">
-							<a href="./imgs/persons/gruber_horst.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+							<a href="./imgs/persons/gruber_horst.jpg" data-toggle="lightbox" data-gallery="multiimages"
 								data-title="Zeugwart: Horst Gruber">
 								<img class="img-thumbnail img-responsive" src="imgs/persons/gruber_horst.jpg"/>
 							</a>
@@ -351,15 +377,15 @@
 						</div>
 					</div>
 				</div>
-				
+
 				<!-- honorary members -->
 				<div class="col-sm-12 text-center top-space">
 					<h3 class="btn btn-danger">Ehrenmitglieder</h3>
 				</div>
-				
+
 				<div class="row">
 					<div class="col-sm-3">
-						<a href="./imgs/persons/hofinger_peter.jpg" data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/hofinger_peter.jpg" data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Peter Hofinger">
 							<img class="img-thumbnail img-responsive" src="imgs/persons/hofinger_peter.jpg"/>
 						</a>
@@ -372,7 +398,7 @@
 						<br />&nbsp;
 					</div>
 					<div class="col-sm-3">
-						<a href="./imgs/persons/berschl_johann.jpg" data-toggle="lightbox" 
+						<a href="./imgs/persons/berschl_johann.jpg" data-toggle="lightbox"
 							data-gallery="multiimages" data-title="Johann Berschl">
 							<img class="img-thumbnail img-responsive" src="imgs/persons/berschl_johann.jpg"/>
 						</a>
@@ -389,12 +415,12 @@
 				<div class="col-sm-12 text-center top-space">
 					<h3 class="btn btn-danger">Fahrer</h3>
 				</div>
-				
+
 				<div class="row">
 					<!-- andreas preishuber -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/anonym.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/anonym.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Andreas Preishuber">
 							<img class="img-thumbnail" src="imgs/persons/anonym.jpg"/>
 						</a>
@@ -402,8 +428,8 @@
 					</div>
 					<!-- christoph preishuber -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/anonym.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/anonym.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Christop Preishuber">
 							<img class="img-thumbnail" src="imgs/persons/anonym.jpg"/>
 						</a>
@@ -411,8 +437,8 @@
 					</div>
 					<!-- mario preishuber -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/preishuber_mario.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/preishuber_mario.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Mario Preishuber">
 							<img class="img-thumbnail" src="imgs/persons/preishuber_mario.jpg"/>
 						</a>
@@ -420,8 +446,8 @@
 					</div>
 					<!-- engelbert salomon -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/salomon_engelbert.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/salomon_engelbert.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Daniel Wiel&auml;nder">
 							<img class="img-thumbnail" src="imgs/persons/salomon_engelbert.jpg"/>
 						</a>
@@ -432,7 +458,7 @@
 					<!-- markus schwarz -->
 					<div class="col-sm-3">
 						<a href="./imgs/persons/schwarz_markus.jpg"
-							data-toggle="lightbox" data-gallery="multiimages" 
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Markus Schwarz">
 							<img class="img-thumbnail" src="imgs/persons/schwarz_markus.jpg"/>
 						</a>
@@ -440,8 +466,8 @@
 					</div>
 					<!-- roland wagner -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/wagner_roland.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/wagner_roland.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Roland Wagner">
 							<img class="img-thumbnail" src="imgs/persons/wagner_roland.jpg"/>
 						</a>
@@ -449,8 +475,8 @@
 					</div>
 					<!-- daniel wielaender -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/anonym.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/anonym.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Daniel Wiel&auml;nder">
 							<img class="img-thumbnail" src="imgs/persons/anonym.jpg"/>
 						</a>
@@ -458,8 +484,8 @@
 					</div>
 					<!-- markus zechmeister -->
 					<div class="col-sm-3">
-						<a href="./imgs/persons/zechmeister_markus.jpg" 
-							data-toggle="lightbox" data-gallery="multiimages" 
+						<a href="./imgs/persons/zechmeister_markus.jpg"
+							data-toggle="lightbox" data-gallery="multiimages"
 							data-title="Markus Zechmeister">
 							<img class="img-thumbnail" src="imgs/persons/zechmeister_markus.jpg"/>
 						</a>
@@ -468,19 +494,19 @@
 				</div>
 			</div>
 		</section>
-			
+
 		<!-- ************************************************************************************ -->
 		<!-- * CONTACT / FOOTER -->
 		<!-- ************************************************************************************ -->
 		<div class="footer my_section" id="contact">
             <div class="container">
-				
+
 				<div class="row">
 					<div class="col-sm-12 text-center top-space">
 						<h3 class="btn btn-danger">Kontakt</h3>
 					</div>
 				</div>
-					
+
 				<div class="row">
 					<div class="col-sm-1">&nbsp;</div>
 					<div class="col-sm-1">
@@ -540,9 +566,9 @@
                 </div>
             </div>
 		</div>
-		
+
 		</div><!-- end content -->
-		
+
 		<!-- ************************************************************************************ -->
 		<!-- * ADDITIONAL CONTENT -->
 		<!-- ************************************************************************************ -->
@@ -554,23 +580,23 @@
 		  js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&appId=481834645275259&version=v2.0";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
-		
+
 		<?php
 		if(!$detect->isMobile()) {
 		?>
 		<script>
-		
+
 		function resizeSections() {
 			if(window.screen.width > 480) {
 				 document.getElementById('home').style.minHeight = window.screen.height + "px";
 				 document.getElementById('calendar').style.minHeight = window.screen.height + "px";
 			}
 		}
-		
+
 		resizeSections();
 		</script>
 		<?php } ?>
-		
+
 		<!-- lightbox - images -->
 		<script type="text/javascript">
 			$(document).ready(function ($) {
@@ -583,11 +609,11 @@
 				});
 			});
 		</script>
-		
+
 		<!-- navigation -->
 		<script>
 			$(function() {
-				$('.nav a').on('click', function(){ 
+				$('.nav a').on('click', function(){
 					if($('.navbar-toggle').css('display') !='none')
 					{
 						$(".navbar-toggle").trigger( "click" );
